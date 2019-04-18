3e676114a708a9483297d8408f0df5185822276c
Lova Linfeldt <lova@Lovas-MacBook-Pro.local>
2019-04-18 17:35:57 +0200

    prog

--- src/componenter/todo.js
+++ src/componenter/todo.js
@@ -12,31 +12,47 @@ class Todo extends Component {
         this.state = {
             token: token$.value,
             todos:[],
-            value: ""
+            value: ''
         };
+
       }
 
-    getData(token){
-      console.log(token);
-      let root = "http://ec2-13-53-32-89.eu-north-1.compute.amazonaws.com:3000";
 
-      axios.get( root + "/todos", {
-         headers: {
-         Authorization: "Bearer" + token,
-        }
-      .then (res => {
-        console.log(res.data.todos);
+
+
+      getData(){
+
+          let root = "http://ec2-13-53-32-89.eu-north-1.compute.amazonaws.com:3000";
+          axios.get(root + '/todos', {
+          headers: { Authorization: 'Bearer ' + this.state.token }
         })
-      .catch (error =>{
-        console.log(error)
-      })
-    })
+          .then ( res =>{
+            this.setState({ value:res.data.todos })
+            console.log(res.data.todos);
+          })
+          .catch (err => {
+            console.log("Request canceled", err.message);
+          })
 
-    }
+
+
+   }
 
     componentDidMount(){
-        this.getData();
-    }
+       this.getData();
+
+     }
+
+    componentWillUnmount(){
+       this.getData();
+     }
+
+    onChange(e){
+       this.setState({ value: e.target.value })
+       console.log(this.state)
+     }
+
+
 
 
 
@@ -46,10 +62,11 @@ class Todo extends Component {
 
 
       return (
-        <div>
-          <li></li>
-
-        </div>
+        <div className='todo'>
+        <h1> Todo List </h1>
+          <input onChange={e => this.onChange(e)}  placeholder="what to do?"/>
+          <button> Submit </button>
+           </div>
         )
       }
     }
